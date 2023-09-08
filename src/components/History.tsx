9acea33e9c5672db1e348ec7bc7753851cc300ab
Umm E Khadija <abrarahmaad824@gmail.com>
2023-09-08 06:12:23 -0700

    some changing in the History

--- src/components/History.tsx
+++ src/components/History.tsx
@@ -7,6 +7,7 @@ const History =() => {
          
         <ul id="list" className="list">
         <HistoryItem credit={true} name="Salary"  value="50000"/>
+        <HistoryItem credit={true} name="Website"  value="30000"/>
         <HistoryItem credit={true} name="Compnay"  value="40000"/>
         <HistoryItem credit={false} name="Rent"  value="500"/>
         <HistoryItem credit={false} name="Shopping"  value="5000"/>
